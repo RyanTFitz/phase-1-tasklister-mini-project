46a27c06783db1c2002913703f318083def0b468
Ryan Fitzpatrick <ryantfitzpatrick@yahoo.com>
2022-11-03 09:30:35 -0400

    task lister commit week 1 Thursday 930a

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+// grab element 
+const form = document.querySelector("#create-task-form");
+const userInput = document.querySelector("#new-task-description");
+const ul = document.querySelector("#tasks");
+// create elements to add to our ul
+// get elements 
+// get input values 
+document.addEventListener("submit", (event) => {
+    // getting user input
+    event.preventDefault();
+    const myLi = document.createElement("li");
+    const deleteMe = document.createElement("button");
+    myLi.textContent = userInput.value; 
+    deleteMe.textContent = "X";
+    ul.appendChild(myLi);
+    myLi.append(deleteMe);
+    userInput.value = ""
+    deleteMe.addEventListener("click", (event) => myLi.remove())
+    
+})
+
 });
+
+
+
+
